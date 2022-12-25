4fda58ca9aacdc3551fa5de4f95714c088ff0f9a
Aung Pyae Sone <aungpyaesoneice@gmail.com>
2022-12-25 10:14:40 +0630

    start_react component

---
+++ project1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import Component1 from './Component_1';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World</h1>
+      <Component1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project1/src/Component_1.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Component1(){
+    return(<div>This is our first component <br></br> My name is Aung Pyae Sone</div>
+    )
+}
